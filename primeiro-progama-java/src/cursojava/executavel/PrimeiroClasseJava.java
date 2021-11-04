f2850b3a76462a2520bcdc557a0d5b331ad6f352
bruno <bruno@192.168.56.1>
2021-11-04 17:34:48 -0300

    Criação do Projeto

---
+++ primeiro-progama-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+public class PrimeiroClasseJava {
+	
+	/*main é um método auto executável em java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Hello Word !!!");
+	}
+}
