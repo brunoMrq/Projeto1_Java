f432dd0d3037037342920ee6ab6d6568cf8bbe3c
bruno <bruno@192.168.56.1>
2021-11-04 22:30:21 -0300

    teste as 22:30

--- primeiro-progama-java/src/cursojava/executavel/PrimeiroClasseJava.java
+++ primeiro-progama-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -10,5 +10,24 @@ public class PrimeiroClasseJava {
 		System.out.println("É nois !!!");
 		System.out.println("Blink Do");
 		System.out.println("Verificando as merdas");
+	
+		int variavel;
+		
+		variavel = 1;
+		
+		System.out.println("O número é: " + variavel);
+		
+		double n = 7;
+		double m = 3;
+		double z = (m + n)/1;
+				
+		System.out.println("O número é: " + z);
+		
+		if (z <= 10 && m==2) {
+			System.out.println(" O número é menor que 10");
+		}
+		else {
+		System.out.println("Resultado Inválidos");
+		}
 	}
 }
